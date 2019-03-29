f2a50a0934305cf9de495145fd45e69c07ffd687
isaclindborg <48713351+isaclindborg@users.noreply.github.com>
2019-03-29 14:28:09 +0100

    health/xp
    health/xp

--- budget-rogue-like/Assets/LevelSystem.cs
+++ budget-rogue-like/Assets/LevelSystem.cs
@@ -2,34 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelSystem : MonoBehaviour
+public class LevelSystem : MonoBehaviour            //skapar public klassen Level system(Mono behaviour=unity script)
 {
-    public int XP;
-    public int currentLevel;
+    public int XP;                                  //skapar en variabel XP och säger att den ska inehåla heltal
+    public int currentLevel;                        //skapar en variabel currentlevel
         
     
     // Update is called once per frame
-    void Update()
+    void Update()                                   //kör scriptet och släng allt som kommer tillbacka
     {
-        UpdateXp(5);
-    }    
-    
-    public void UpdateXp(int xp)
+if  (false)                                         //just nu Om inte stämemer kör
+        UpdateXp(5);                            
+    }
+
+    public void UpdateXp(int xp)                   //skapar en variabel (updateXp) som alla kan kalla på
     {
-        XP  += xp;
+        XP  += xp;                                 //säger att XP=XP+xp
 
-        int curlvl = (int)(0.1f * Mathf.Sqrt(XP));
+        int curlvl = (int)(0.1f * Mathf.Sqrt(XP));  //heltalet curlvl = 0,1* roten ur XP
 
-        if(curlvl != currentLevel)
+        if(curlvl != currentLevel)                  //om curlvl inte = currentlevel 
         {
-            currentLevel = curlvl;
+            currentLevel = curlvl;                  //så är currentlevel = curlvl
             //level text
         } 
 
-        int xpnextlevel = 100 * (currentLevel +1) * (currentLevel + 1);
-        int differencexp = xpnextlevel - xp;
+        int xpnextlevel = 100 * (currentLevel +1) * (currentLevel + 1);     //heltalet xpnextlevel = 100 * din level + 1 * din level + 1
+        int differencexp = xpnextlevel - xp;                                //heltalet differencexp = all xp från 0 - nästa level - xpn till nästa level
 
-        int totaldifference = xpnextlevel -(100 * currentLevel * currentLevel);
+        int totaldifference = xpnextlevel -(100 * currentLevel * currentLevel);     //heltalet totaldifference = xp till nästa level -(100 * din level just nu * din level just nu)
     }   
 }
 
