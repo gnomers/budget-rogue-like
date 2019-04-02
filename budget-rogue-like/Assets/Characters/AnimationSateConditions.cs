4de1415cf79276dd0f57761abdee88cb5f14011e
Klaypzz <44588047+Klaypzz@users.noreply.github.com>
2019-04-02 11:45:01 +0200

    sda

--- budget-rogue-like/Assets/Characters/AnimationSateConditions.cs
+++ budget-rogue-like/Assets/Characters/AnimationSateConditions.cs
@@ -9,6 +9,7 @@ private Animator anim;
     // Start is called before the first frame update
     void Start() {
         anim = GetComponent<Animator>();
+        
     }
 
     // Update is called once per frame
@@ -18,5 +19,7 @@ private Animator anim;
         } else {
             anim.SetBool("idle", true);
         }
+
+
     }
 }
