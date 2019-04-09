5817740f7d57c00946f785a32fb76c3cc3149d65
isaclindborg <48713351+isaclindborg@users.noreply.github.com>
2019-04-09 13:13:11 +0200

    enemy_movement

---
+++ budget-rogue-like/Assets/Enemy_movement.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_movement : MonoBehaviour
+
+    public Currentlocation
+{
+    public GameObject findClosestPlayer();
+
+}
