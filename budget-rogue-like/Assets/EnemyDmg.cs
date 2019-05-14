6f6bdbcfa4c81429174e39c4812b8cc027af01e3
isaclindborg <48713351+isaclindborg@users.noreply.github.com>
2019-05-14 15:39:26 +0200

    health
    health

---
+++ budget-rogue-like/Assets/EnemyDmg.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDmg : MonoBehaviour
+{
+    private float timer = 0f;
+    public Transform SpawnPoint;
+        void Update()
+    {
+        if(timer > 0) 
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "Player") {
+            if(timer <= 0) {
+                GameObject.Find("The Mad Lad").GetComponent<PlayerHealth>().Health --;
+                timer = 1f;
+                Debug.Log(GameObject.Find("The Mad Lad").GetComponent<PlayerHealth>().Health);
+                if (GameObject.Find("The Mad Lad").GetComponent<PlayerHealth>().Health <= 0)
+                {
+                    GameObject.Find("The Mad Lad").GetComponent<PlayerHealth>().Death();
+                } 
+            }
+        }
+    }
+}
--- budget-rogue-like/Assets/PlayerHealth.cs
+++ budget-rogue-like/Assets/PlayerHealth.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public float Health = 5f;
-    private float timer = 0f;
     // Start is called before the first frame update
 
     public Transform SpawnPoint;
@@ -15,13 +14,17 @@ public class PlayerHealth : MonoBehaviour
     }
 
     // Update is called once per frame
-    void OnCollisionEnter2D(Collision2D other)
+
+    public void Death() 
     {
-    
-        if(other.transform.tag == "enemy")
+        transform.position = SpawnPoint.transform.position;
+        Health = 5f;
+    }
+  /*  void dmg()
+        {
+         
         {
             if(timer <= 0) {
-                Debug.Log(other.transform.tag);
                 Health -= 1;
                 timer = 1f;
                 if (Health <= 0)
@@ -30,17 +33,5 @@ public class PlayerHealth : MonoBehaviour
                 } 
             }
         } 
-    }
-    public void Death() 
-    {
-        transform.position = SpawnPoint.transform.position;
-        Health = 5f;
-    }
-    void Update()
-    {
-        if(timer > 0) 
-        {
-            timer -= Time.deltaTime;
-        }
-    }
+    } */
 } 
